39cee9922f43885e8a2d42987f513a50b59c8139
Moritz Schönherr <moritz.schoenherr@gmail.com>
2018-01-07 16:45:02 +0100

    initial commit with database model

---
+++ db_setup.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+
+    def serialize(self):
+        return { 'id' : self.id,
+                 'name': self.name}
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable = False)
+    description = Column(String)
+
+    def serialize(self):
+        return { 'id': self.id,
+                 'name': self.name,
+                 'category_id': self.category_id,
+                 'description': self.description }
+                 
